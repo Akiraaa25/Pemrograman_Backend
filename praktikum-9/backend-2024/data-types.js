f66d6973c684724274caf2d28cc9c1de129c71e0
Akiraaa25 <146069139+Akiraaa25@users.noreply.github.com>
2024-12-12 17:48:47 +0700

    Update data-types.js

--- praktikum-9/backend-2024/data-types.js
+++ praktikum-9/backend-2024/data-types.js
@@ -1,5 +1,5 @@
-const nama = "Edo Riansyah"; // string
-const umur = 34; // number
+const nama = "Hasan AL Banna"; // string
+const umur = 19; // number
 const sudahMenikah = true; // boolean
 const x = null; // null
 const y = undefined; // undefined
