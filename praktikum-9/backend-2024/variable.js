c4f31f26d8a3b2dca08b6411063faf4044e4db7f
Akiraaa25 <146069139+Akiraaa25@users.noreply.github.com>
2024-12-12 17:47:17 +0700

    Update variable.js

--- praktikum-9/backend-2024/variable.js
+++ praktikum-9/backend-2024/variable.js
@@ -2,8 +2,8 @@
  * Membuat variable dengan keyword const
  * Variable (const) tidak dapat diubah nilainya
  */
-const nama = "Edo Riansyah";
-const umur = 34;
+const nama = "Hasan AL Banna";
+const umur = 19;
 const jurusan = "Teknik Informatika";
 
 console.log(nama, umur, jurusan);
