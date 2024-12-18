8e2e2fc81484c19d7b4cbc3f3e4aab096db72efd
Akiraaa25 <146069139+Akiraaa25@users.noreply.github.com>
2024-12-18 15:33:35 +0700

    Delete praktikum-9/backend-2024 directory

--- praktikum-9/backend-2024/variable.js
+++
@@ -1,9 +0,0 @@
-/**
- * Membuat variable dengan keyword const
- * Variable (const) tidak dapat diubah nilainya
- */
-const nama = "Hasan AL Banna";
-const umur = 19;
-const jurusan = "Teknik Informatika";
-
-console.log(nama, umur, jurusan);
