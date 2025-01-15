7a767a5678ceaf63d7d2f2ed4dbf01f37bd11cce
Akiraaa25 <146069139+Akiraaa25@users.noreply.github.com>
2025-01-15 15:22:53 +0700

    Delete src/validators directory

--- src/validators/covidValidator.js
+++
@@ -1,8 +0,0 @@
-const { body } = require('express-validator');
-
-exports.validateCovidData = [
-    body('name').notEmpty().withMessage('Name is required'),
-    body('phone').notEmpty().withMessage('Phone is required'),
-    body('address').notEmpty().withMessage('Address is required'),
-    body('status').notEmpty().withMessage('Status is required')
-];
