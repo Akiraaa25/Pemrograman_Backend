42397abb0fc6b03793bc7ea78b9ba91b16791fa5
Akiraaa25 <146069139+Akiraaa25@users.noreply.github.com>
2025-01-15 15:17:02 +0700

    Delete database/migrations directory

--- database/migrations/create_covid_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE covid (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  phone VARCHAR(20) NOT NULL,
-  address TEXT NOT NULL,
-  status ENUM('positive', 'recovered', 'dead') NOT NULL,
-  out_date_at DATE
-);
